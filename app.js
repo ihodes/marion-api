62de56055b1e5c57532dd6edc732f1be6870adfc
Isaac Hodes <ihodes@mac.com>
2013-09-09 21:18:18 -0400

    Added in route namespacing (this is a joy).

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 'use strict';
 
 
-// Requires
-var express = require('express'),
-    config  = require('./config');
+// Required
+var express     = require('express'),
+    __namespace = require('express-namespace'),
+    config      = require('./config');
 
 // Routes
 var people    = require('./routes/people'),
@@ -14,51 +15,49 @@ var people    = require('./routes/people'),
     protocols = require('./routes/protocols'),
     states    = require('./routes/states');
 
-
-
 // Globals
 var app = express();
 
 
-// Routing functions
-var root = function (req, res) {
-    return res.send({message: "See api docs at marion.io/api"});
-};
-
-
 // Request Routing
-app.get('/', root);
+app.get('/', function (req, res) { res.send({message: "See api docs at marion.io/api"}); });
 
-app.get('/v1/people', people.getPeople)
-app.post('/v1/people', people.createPerson)
-app.get('/v1/person/:personid', people.getPerson)
-app.post('/v1/person/:personid', people.updatePerson)
-app.delete('/v1/person/:personpid', people.deletePerson)
-
-app.get('/v1/person/:personid/schedules', schedules.getSchedules)
-app.post('/v1/person/:personid/schedules', schedules.createSchedule)
-app.get('/v1/person/:personid/schedule/:scheduleid', schedules.getSchedule)
-app.post('/v1/person/:personid/schedule/:scheduleid', schedules.updateSchedule)
-app.delete('/v1/person/:personid/schedule/:scheduleid', schedules.deleteSchedule)
-
-app.get('/v1/person/:personid/responses', responses.getResponses)
-app.post('/v1/person/:personid/responses', responses.createResponse)
-app.get('/v1/person/:personid/response/:responseid', responses.getResponse)
-app.post('/v1/person/:personid/response/:responseid', responses.updateResponse)
-app.delete('/v1/person/:personid/response/:responseid', responses.deleteResponse)
-
-app.get('/v1/protocols', protocols.getProtocols)
-app.post('/v1/protocols', protocols.createProtocol)
-app.get('/v1/protocol/:protocolid', protocols.getProtocol)
-app.post('/v1/protocol/:protocolid', protocols.updateProtocol)
-app.delete('/v1/protocol/:protocolpid', protocols.deleteProtocol)
-
-app.get('/v1/protocol/:protocolid/states', states.getStates)
-app.post('/v1/protocol/:protocolid/states', states.createState)
-app.get('/v1/protocol/:protocolid/state/:stateid', states.getState)
-app.post('/v1/protocol/:protocolid/state/:stateid', states.updateState)
-app.delete('/v1/protocol/:protocolid/state/:stateid', states.deleteState)
-
+app.namespace('/v1', function () {
+    app.get('/people', people.getPeople);
+    app.post('/people', people.createPerson);
+    app.get('/person/:personid', people.getPerson);
+    app.post('/person/:personid', people.updatePerson);
+    app.delete('/person/:personpid', people.deletePerson);
+    
+    app.namespace('/person/:personid', function () {
+        app.get('/schedules', schedules.getSchedules);
+        app.post('/schedules', schedules.createSchedule);
+        app.get('/schedule/:scheduleid', schedules.getSchedule);
+        app.post('/schedule/:scheduleid', schedules.updateSchedule);
+        app.delete('/schedule/:scheduleid', schedules.deleteSchedule);
+        
+        app.get('/responses', responses.getResponses);
+        app.post('/responses', responses.createResponse);
+        app.get('/response/:responseid', responses.getResponse);
+        app.post('/response/:responseid', responses.updateResponse);
+        app.delete('/response/:responseid', responses.deleteResponse);
+    });
+    
+    app.get('/protocols', protocols.getProtocols);
+    app.post('/protocols', protocols.createProtocol);
+    app.get('/protocol/:protocolid', protocols.getProtocol);
+    app.post('/protocol/:protocolid', protocols.updateProtocol);
+    app.delete('/protocol/:protocolpid', protocols.deleteProtocol);
+    
+    app.namespace('/protocol/:protocolid', function () {
+        app.get('/states', states.getStates);
+        app.post('/states', states.createState);
+        app.get('/state/:stateid', states.getState);
+        app.post('/state/:stateid', states.updateState);
+        app.delete('/state/:stateid', states.deleteState);
+    });
+})
+app.all('*', function (req, res) { res.status(404).send({message: "Not Found."}); } );
 
 // App server setup
 app.listen(config.settings.PORT, function () {
